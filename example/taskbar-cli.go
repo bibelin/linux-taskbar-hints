f9a11ef9b71eed096d6ae5bc9f7a2f5616c3d38f
Balian of Ibelin <balian1belin@yandex.ru>
2024-10-01 08:42:08 +0300

    Add `-demo` to show animation

--- example/taskbar-cli.go
+++ example/taskbar-cli.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"math"
+	"time"
 
 	"github.com/bibelin/taskbar"
 )
@@ -13,6 +15,7 @@ func main() {
 	progressPtr := flag.Int("progress", 0, "Progress value (0-100)")
 	pulsePtr := flag.Bool("pulse", false, "Pulse state")
 	countPtr := flag.Int("count", 0, "Counter value")
+	demoPtr := flag.Bool("demo", false, "Runs demonstration. Properties flags will be ignored.")
 	flag.Parse()
 
 	tb, err := taskbar.Connect(*desktopPtr, *xidPtr)
@@ -30,13 +33,34 @@ func main() {
 	// 	}
 	// }()
 
-	if err := tb.SetProgress(*progressPtr); err != nil {
+	if *demoPtr {
+		fmt.Println("Starting demonstration...")
+		for i := 0; i < 100; i++ {
+			progress := int(math.Floor(float64(i)/10.0) * 10)
+			update(tb, progress, false, i)
+			time.Sleep(100 * time.Millisecond)
+		}
+		pulse := true
+		for i := 0; i < 6; i++ {
+			update(tb, 0, pulse, 0)
+			pulse = !pulse
+			time.Sleep(1 * time.Second)
+		}
+		tb.Disconnect()
+		fmt.Println("Finish")
+	} else {
+		update(tb, *progressPtr, *pulsePtr, *countPtr)
+	}
+}
+
+func update(tb *taskbar.Taskbar, progress int, pulse bool, count int) {
+	if err := tb.SetProgress(progress); err != nil {
 		fmt.Println(err)
 	}
-	if err := tb.SetPulse(*pulsePtr); err != nil {
+	if err := tb.SetPulse(pulse); err != nil {
 		fmt.Println(err)
 	}
-	if err := tb.SetCount(*countPtr); err != nil {
+	if err := tb.SetCount(count); err != nil {
 		fmt.Println(err)
 	}
 }
